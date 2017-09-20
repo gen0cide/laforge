0fe711d62a878ce0e6b590e38bdef08a83b72a1d
Alex Levinson <alexl@uber.com>
2017-09-19 19:28:22 -0700

    this time?

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package main
 
 // const Name string = "laforge"
 // const Version string = "1.0.2"
-var Name string
+var Name = "laforge"
 var Version string
 var ReleaseDate string
 var CommitRef string
