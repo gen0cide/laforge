cfb49d91b492fa9a30900b6e338faa89df3a7436
Connor Leavesley <cxl6834@rit.edu>
2020-12-21 14:50:24 -0500

    Finished about half the graphql resolvers.

--- graphql/graph/schema.resolvers.go
+++ graphql/graph/schema.resolvers.go
@@ -7,8 +7,14 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/gen0cide/laforge/ent"
+	"github.com/gen0cide/laforge/ent/build"
+	"github.com/gen0cide/laforge/ent/environment"
+	"github.com/gen0cide/laforge/ent/provisionedhost"
+	"github.com/gen0cide/laforge/ent/provisionednetwork"
+	"github.com/gen0cide/laforge/ent/provisioningstep"
 	"github.com/gen0cide/laforge/graphql/graph/generated"
 	"github.com/gen0cide/laforge/graphql/graph/model"
 )
@@ -153,181 +159,417 @@ func (r *hostResolver) Scripts(ctx context.Context, obj *ent.Host) ([]*ent.Scrip
 	panic(fmt.Errorf("not implemented"))
 }
 
+// Not Implimented
 func (r *hostResolver) FileDeletes(ctx context.Context, obj *ent.Host) ([]*ent.FileDelete, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
+// Not Implimented
 func (r *hostResolver) FileDownloads(ctx context.Context, obj *ent.Host) ([]*ent.FileDownload, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
+// Not Implimented
 func (r *hostResolver) FileExtracts(ctx context.Context, obj *ent.Host) ([]*ent.FileExtract, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
 func (r *mutationResolver) ExecutePlan(ctx context.Context, buildUUID string) (*ent.Build, error) {
-	panic(fmt.Errorf("not implemented"))
+	uuid, err := strconv.Atoi(buildUUID)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed casting UUID to int: %v", err)
+	}
+
+	b, err := r.client.Build.Query().Where(build.IDEQ(uuid)).Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying Build: %v", err)
+	}
+
+	log.Println("Build returned: ", b)
+	return b, nil
 }
 
 func (r *networkResolver) Vars(ctx context.Context, obj *ent.Network) ([]*model.VarsMap, error) {
-	panic(fmt.Errorf("not implemented"))
+	results := make([]*model.VarsMap, 0)
+
+	for k, v := range obj.Vars {
+		results = append(results, &model.VarsMap{k, v})
+	}
+
+	return results, nil
 }
 
 func (r *networkResolver) Tags(ctx context.Context, obj *ent.Network) ([]*ent.Tag, error) {
-	panic(fmt.Errorf("not implemented"))
+	t, err := obj.QueryTag().All(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying Tags: %v", err)
+	}
+
+	log.Println("Tags returned: ", t)
+	return t, nil
 }
 
 func (r *provisionedHostResolver) Status(ctx context.Context, obj *ent.ProvisionedHost) (*ent.Status, error) {
-	panic(fmt.Errorf("not implemented"))
+	s, err := obj.QueryStatus().Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying Status: %v", err)
+	}
+
+	log.Println("Status returned: ", s)
+	return s, nil
 }
 
 func (r *provisionedHostResolver) ProvisionedNetwork(ctx context.Context, obj *ent.ProvisionedHost) (*ent.ProvisionedNetwork, error) {
-	panic(fmt.Errorf("not implemented"))
+	pn, err := obj.QueryProvisionedNetwork().Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying ProvisionedNetwork: %v", err)
+	}
+
+	log.Println("ProvisionedNetwork returned: ", pn)
+	return pn, nil
 }
 
 func (r *provisionedHostResolver) Host(ctx context.Context, obj *ent.ProvisionedHost) (*ent.Host, error) {
-	panic(fmt.Errorf("not implemented"))
+	h, err := obj.QueryHost().Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying Host: %v", err)
+	}
+
+	log.Println("Host returned: ", h)
+	return h, nil
 }
 
+// TODO
 func (r *provisionedHostResolver) CombinedOutput(ctx context.Context, obj *ent.ProvisionedHost) (*string, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
+// TODO
 func (r *provisionedHostResolver) Heartbeat(ctx context.Context, obj *ent.ProvisionedHost) (*ent.AgentStatus, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
+// Not Implimented
 func (r *provisionedNetworkResolver) Vars(ctx context.Context, obj *ent.ProvisionedNetwork) ([]*model.VarsMap, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
+// Not Implimented
 func (r *provisionedNetworkResolver) Tags(ctx context.Context, obj *ent.ProvisionedNetwork) ([]*ent.Tag, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
 func (r *provisionedNetworkResolver) Status(ctx context.Context, obj *ent.ProvisionedNetwork) (*ent.Status, error) {
-	panic(fmt.Errorf("not implemented"))
+	s, err := obj.QueryStatus().Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying Status: %v", err)
+	}
+
+	log.Println("Status returned: ", s)
+	return s, nil
 }
 
 func (r *provisionedNetworkResolver) Network(ctx context.Context, obj *ent.ProvisionedNetwork) (*ent.Network, error) {
-	panic(fmt.Errorf("not implemented"))
+	n, err := obj.QueryNetwork().Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying Network: %v", err)
+	}
+
+	log.Println("Network returned: ", n)
+	return n, nil
 }
 
 func (r *provisionedNetworkResolver) Build(ctx context.Context, obj *ent.ProvisionedNetwork) (*ent.Build, error) {
-	panic(fmt.Errorf("not implemented"))
+	b, err := obj.QueryBuild().Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying Build: %v", err)
+	}
+
+	log.Println("Build returned: ", b)
+	return b, nil
 }
 
 func (r *provisioningStepResolver) ProvisionType(ctx context.Context, obj *ent.ProvisioningStep) (string, error) {
-	panic(fmt.Errorf("not implemented"))
+	return obj.ProvisionerType, nil
 }
 
 func (r *provisioningStepResolver) ProvisionedHost(ctx context.Context, obj *ent.ProvisioningStep) (*ent.ProvisionedHost, error) {
-	panic(fmt.Errorf("not implemented"))
+	ph, err := obj.QueryProvisionedHost().Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying ProvisionedHost: %v", err)
+	}
+
+	log.Println("ProvisionedHost returned: ", ph)
+	return ph, nil
 }
 
+// Not Implimented
 func (r *provisioningStepResolver) Status(ctx context.Context, obj *ent.ProvisioningStep) (*ent.Status, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
 func (r *provisioningStepResolver) Script(ctx context.Context, obj *ent.ProvisioningStep) (*ent.Script, error) {
-	panic(fmt.Errorf("not implemented"))
+	s, err := obj.QueryScript().Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying Script: %v", err)
+	}
+
+	log.Println("Script returned: ", s)
+	return s, nil
 }
 
 func (r *provisioningStepResolver) Command(ctx context.Context, obj *ent.ProvisioningStep) (*ent.Command, error) {
-	panic(fmt.Errorf("not implemented"))
+	c, err := obj.QueryCommand().Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying Command: %v", err)
+	}
+
+	log.Println("Command returned: ", c)
+	return c, nil
 }
 
 func (r *provisioningStepResolver) DNSRecord(ctx context.Context, obj *ent.ProvisioningStep) (*ent.DNSRecord, error) {
-	panic(fmt.Errorf("not implemented"))
+	d, err := obj.QueryDNSRecord().Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying DNSRecord: %v", err)
+	}
+
+	log.Println("DNSRecord returned: ", d)
+	return d, nil
 }
 
+// Not Implimented
 func (r *provisioningStepResolver) FileDownload(ctx context.Context, obj *ent.ProvisioningStep) (*ent.FileDownload, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
+// Not Implimented
 func (r *provisioningStepResolver) FileDelete(ctx context.Context, obj *ent.ProvisioningStep) (*ent.FileDelete, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
+// Not Implimented
 func (r *provisioningStepResolver) FileExtract(ctx context.Context, obj *ent.ProvisioningStep) (*ent.FileExtract, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
 func (r *queryResolver) Environments(ctx context.Context) ([]*ent.Environment, error) {
 	e, err := r.client.Environment.Query().All(ctx)
+
 	if err != nil {
-        return nil, fmt.Errorf("failed querying Environment: %v", err)
-    }
-    log.Println("Environment returned: ", e)
-    return e, nil
+		return nil, fmt.Errorf("failed querying Environment: %v", err)
+	}
+
+	log.Println("Environment returned: ", e)
+	return e, nil
 }
 
 func (r *queryResolver) Environment(ctx context.Context, envUUID string) (*ent.Environment, error) {
-	panic(fmt.Errorf("not implemented"))
+	uuid, err := strconv.Atoi(envUUID)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed casting UUID to int: %v", err)
+	}
+
+	e, err := r.client.Environment.Query().Where(environment.IDEQ(uuid)).Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying Environment: %v", err)
+	}
+
+	log.Println("Environment returned: ", e)
+	return e, nil
 }
 
 func (r *queryResolver) ProvisionedHost(ctx context.Context, proHostUUID string) (*ent.ProvisionedHost, error) {
-	panic(fmt.Errorf("not implemented"))
+	uuid, err := strconv.Atoi(proHostUUID)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed casting UUID to int: %v", err)
+	}
+
+	ph, err := r.client.ProvisionedHost.Query().Where(provisionedhost.IDEQ(uuid)).Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying ProvisionedHost: %v", err)
+	}
+
+	log.Println("ProvisionedHost returned: ", ph)
+	return ph, nil
 }
 
 func (r *queryResolver) ProvisionedNetwork(ctx context.Context, proNetUUID string) (*ent.ProvisionedNetwork, error) {
-	panic(fmt.Errorf("not implemented"))
+	uuid, err := strconv.Atoi(proNetUUID)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed casting UUID to int: %v", err)
+	}
+
+	pn, err := r.client.ProvisionedNetwork.Query().Where(provisionednetwork.IDEQ(uuid)).Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying ProvisionedNetwork: %v", err)
+	}
+
+	log.Println("ProvisionedNetwork returned: ", pn)
+	return pn, nil
 }
 
 func (r *queryResolver) ProvisionedStep(ctx context.Context, proStepUUID string) (*ent.ProvisioningStep, error) {
-	panic(fmt.Errorf("not implemented"))
+	uuid, err := strconv.Atoi(proStepUUID)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed casting UUID to int: %v", err)
+	}
+
+	ps, err := r.client.ProvisioningStep.Query().Where(provisioningstep.IDEQ(uuid)).Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying ProvisionedStep: %v", err)
+	}
+
+	log.Println("ProvisionedStep returned: ", ps)
+	return ps, nil
 }
 
 func (r *scriptResolver) Vars(ctx context.Context, obj *ent.Script) ([]*model.VarsMap, error) {
-	panic(fmt.Errorf("not implemented"))
+	results := make([]*model.VarsMap, 0)
+
+	for k, v := range obj.Vars {
+		results = append(results, &model.VarsMap{k, v})
+	}
+
+	return results, nil
 }
 
 func (r *scriptResolver) Tags(ctx context.Context, obj *ent.Script) ([]*ent.Tag, error) {
-	panic(fmt.Errorf("not implemented"))
+	t, err := obj.QueryTag().All(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying Tag: %v", err)
+	}
+
+	log.Println("Tag returned: ", t)
+	return t, nil
 }
 
 func (r *scriptResolver) Maintainer(ctx context.Context, obj *ent.Script) (*ent.User, error) {
-	panic(fmt.Errorf("not implemented"))
+	u, err := obj.QueryMaintainer().Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying User: %v", err)
+	}
+
+	log.Println("User returned: ", u)
+	return u, nil
 }
 
 func (r *scriptResolver) Findings(ctx context.Context, obj *ent.Script) ([]*ent.Finding, error) {
-	panic(fmt.Errorf("not implemented"))
+	f, err := obj.QueryFinding().All(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying Finding: %v", err)
+	}
+
+	log.Println("Finding returned: ", f)
+	return f, nil
 }
 
 func (r *statusResolver) State(ctx context.Context, obj *ent.Status) (model.ProvisionStatus, error) {
-	panic(fmt.Errorf("not implemented"))
+	switch state := obj.State; state {
+	case "AWAITING":
+		return model.ProvisionStatusProvStatusAwaiting, nil
+	case "INPROGRESS":
+		return model.ProvisionStatusProvStatusInProgress, nil
+	case "FAILED":
+		return model.ProvisionStatusProvStatusFailed, nil
+	case "COMPLETE":
+		return model.ProvisionStatusProvStatusComplete, nil
+	case "TAINTED":
+		return model.ProvisionStatusProvStatusTainted, nil
+	default:
+		return model.ProvisionStatusProvStatusUndefined, nil
+	}
 }
 
 func (r *statusResolver) StartedAt(ctx context.Context, obj *ent.Status) (string, error) {
-	panic(fmt.Errorf("not implemented"))
+	return obj.StartedAt.String(), nil
 }
 
 func (r *statusResolver) EndedAt(ctx context.Context, obj *ent.Status) (string, error) {
-	panic(fmt.Errorf("not implemented"))
+	return obj.EndedAt.String(), nil
 }
 
 func (r *tagResolver) Description(ctx context.Context, obj *ent.Tag) (*string, error) {
-	panic(fmt.Errorf("not implemented"))
+	desc := fmt.Sprint(obj.Description)
+	return &desc, nil
 }
 
 func (r *teamResolver) Config(ctx context.Context, obj *ent.Team) ([]*model.ConfigMap, error) {
-	panic(fmt.Errorf("not implemented"))
+	results := make([]*model.ConfigMap, 0)
+
+	for k, v := range obj.Config {
+		results = append(results, &model.ConfigMap{k, v})
+	}
+
+	return results, nil
 }
 
 func (r *teamResolver) Maintainer(ctx context.Context, obj *ent.Team) (*ent.User, error) {
-	panic(fmt.Errorf("not implemented"))
+	m, err := obj.QueryMaintainer().Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying User: %v", err)
+	}
+
+	log.Println("User returned: ", m)
+	return m, nil
 }
 
 func (r *teamResolver) Build(ctx context.Context, obj *ent.Team) (*ent.Build, error) {
-	panic(fmt.Errorf("not implemented"))
+	b, err := obj.QueryBuild().Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying Build: %v", err)
+	}
+
+	log.Println("Build returned: ", b)
+	return b, nil
 }
 
 func (r *teamResolver) Environment(ctx context.Context, obj *ent.Team) (*ent.Environment, error) {
-	panic(fmt.Errorf("not implemented"))
+	e, err := obj.QueryTeamToEnvironment().Only(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying Environment: %v", err)
+	}
+
+	log.Println("Environment returned: ", e)
+	return e, nil
 }
 
 func (r *teamResolver) Tags(ctx context.Context, obj *ent.Team) ([]*ent.Tag, error) {
-	panic(fmt.Errorf("not implemented"))
+	t, err := obj.QueryTag().All(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed querying Tag: %v", err)
+	}
+
+	log.Println("Tag returned: ", t)
+	return t, nil
 }
 
 // Build returns generated.BuildResolver implementation.
