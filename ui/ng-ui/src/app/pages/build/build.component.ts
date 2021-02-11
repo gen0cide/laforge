b4c707d50fe87d002d175be375b44d55791967a7
BradHacker <harkerbd@gmail.com>
2021-02-11 20:08:43 +0000

    Scaffold build page

--- ui/ng-ui/src/app/pages/build/build.component.ts
+++ ui/ng-ui/src/app/pages/build/build.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Environment } from 'src/app/models/environment.model';
+import { EnvironmentService } from 'src/app/services/environment/environment.service';
+import { SubheaderService } from 'src/app/_metronic/partials/layout/subheader/_services/subheader.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-build',
@@ -6,7 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./build.component.scss']
 })
 export class BuildComponent implements OnInit {
-  constructor() {}
+  environment: Observable<Environment>;
+
+  constructor(private subheader: SubheaderService, public envService: EnvironmentService) {
+    this.subheader.setTitle('Build');
+    this.subheader.setDescription('Build a planned environment');
+
+    this.environment = this.envService.getCurrentEnv().asObservable();
+  }
 
   ngOnInit(): void {}
+
+  envIsSelected(): boolean {
+    return this.envService.getCurrentEnv().getValue() != null;
+  }
 }
