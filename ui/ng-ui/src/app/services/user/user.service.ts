302459f8dfff6852bebf55fbe5aa095cb8632f91
BradHacker <harkerbd@gmail.com>
2021-02-19 00:44:07 +0000

    Scaffold out user service

---
+++ ui/ng-ui/src/app/services/user/user.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { PromiseType } from 'protractor/built/plugins';
+import { User } from 'src/app/models/user.model';
+import { ApiService } from '../api/api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private user: User;
+
+  constructor(private api: ApiService) {
+    // TODO: refresh user session with refreshToken
+  }
+
+  public login(emailOrUsername: string, password: string): Promise<User> {
+    // TODO: Perform user auth
+    return Promise.reject({ message: 'Feature not implemented yet' });
+  }
+
+  public signout() {
+    // TODO: Sign out user by clearing the cookies or something
+    this.user = null;
+    window.location.reload(); // reload window to reset them to auth screen
+  }
+
+  public updateUser(user: User) {
+    // TODO: update the user in the DB with new details
+  }
+
+  public createUser(username: string, email: string, password: string) {
+    // TODO: create the user in the DB
+  }
+
+  public getUsers(): Promise<User[]> {
+    // TODO: Pull a list of the users from the DB
+    return Promise.reject({ message: 'Feature not implemented yet' });
+  }
+
+  public deleteUser(): Promise<{ success: boolean }> {
+    // TODO: Delete the user in the DB (possivle just archive them and not "delete" any data?)
+    return Promise.reject({ message: 'Feature not implemented yet' });
+  }
+}
