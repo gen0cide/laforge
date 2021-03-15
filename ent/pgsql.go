5fed0bfc6dd76c0f0c192b7ba8ab45ecaa8cf8f2
Fred Rybin <red@redrybin.me>
2021-03-15 08:34:25 -0400

    Added DB rate limiting

--- ent/pgsql.go
+++ ent/pgsql.go
@@ -3,6 +3,9 @@ package ent
 import (
 	"database/sql"
 	"log"
+	"os"
+	"strconv"
+	"time"
 
 	"entgo.io/ent/dialect"
 	entsql "entgo.io/ent/dialect/sql"
@@ -15,6 +18,30 @@ func PGOpen(databaseURL string) *Client {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	maxConnections := 10
+	if limit := os.Getenv("PG_CONN_LIMIT"); limit != "" {
+		connLimit, err := strconv.Atoi(limit)
+		if err != nil {
+			return nil
+		} else {
+			maxConnections = connLimit
+		}
+	}
+
+	maxIdle := 10
+	if limit := os.Getenv("PG_IDLE_LIMIT"); limit != "" {
+		idleLimit, err := strconv.Atoi(limit)
+		if err != nil {
+			return nil
+		} else {
+			maxConnections = idleLimit
+		}
+	}
+
+	db.SetMaxIdleConns(maxIdle)
+	db.SetMaxOpenConns(maxConnections)
+	db.SetConnMaxLifetime(time.Hour)
 
 	// Create an ent.Driver from `db`.
 	drv := entsql.OpenDB(dialect.Postgres, db)
