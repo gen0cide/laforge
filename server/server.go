8476bb4a5961bd59c9d4ce47f01c94b1859a52a6
BradHacker <harkerbd@gmail.com>
2021-05-27 21:16:24 +0000

    Temporarily allow duplicate agent donwloading

--- server/server.go
+++ server/server.go
@@ -47,10 +47,10 @@ func tempURLHandler(client *ent.Client) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		urlID := ctx.Param("url_id")
 		fileInfo, err := client.GinFileMiddleware.Query().Where(
-			ginfilemiddleware.And(
-				ginfilemiddleware.URLIDEQ(urlID),
-				ginfilemiddleware.AccessedEQ(false),
-			),
+			// ginfilemiddleware.And(
+			ginfilemiddleware.URLIDEQ(urlID),
+			// 	ginfilemiddleware.AccessedEQ(false),
+			// ),
 		).
 			Only(ctx)
 		if err != nil {
