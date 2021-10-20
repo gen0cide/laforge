630e40a758e931c83e2193a2be209e77ac7f026c
Fred Rybin <red@redrybin.me>
2021-10-20 13:11:46 +0000

    I'm stupid

--- grpc/agent/agent_windows.go
+++ grpc/agent/agent_windows.go
@@ -1,3 +1,4 @@
+//go:build windows
 // +build windows
 
 package main
@@ -106,28 +107,24 @@ func SystemExecuteCommand(command string, args ...string) (string, error) {
 	arguments := []string{}
 	arguments = append(arguments, command)
 	arguments = append(arguments, args...)
-	// cmd := exec.Command("powershell.exe", arguments...)
-	// out, err := cmd.CombinedOutput()
-	// if err == nil {
-	// 	output = string(out)
-	// 	break
+	cmd := exec.Command("powershell.exe", arguments...)
+	out, err := cmd.CombinedOutput()
+	return string(out), err
+	// retryCount := 5
+	// for i := 0; i < retryCount; i++ {
+	// 	// Get the data
+	// 	cmd := exec.Command("powershell.exe", arguments...)
+	// 	out, err := cmd.CombinedOutput()
+	// 	if err == nil {
+	// 		output = string(out)
+	// 		break
+	// 	}
+	// 	time.Sleep(1 * time.Minute)
 	// }
-	// return string(out), err
-	retryCount := 5
-	for i := 0; i < retryCount; i++ {
-		// Get the data
-		cmd := exec.Command("powershell.exe", arguments...)
-		out, err := cmd.CombinedOutput()
-		if err == nil {
-			output = string(out)
-			break
-		}
-		time.Sleep(1 * time.Minute)
-	}
-	if err != nil {
-		return output, err
-	}
-	return output, nil
+	// if err != nil {
+	// 	return output, err
+	// }
+	// return output, nil
 }
 
 func GetSystemDependencies() []string {
